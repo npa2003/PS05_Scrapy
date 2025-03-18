5874fe52d239db2e8d32ea56217c62a855b38e1a
Pavel N <npa2003@gmail.com>
2025-03-18 22:26:27 +0300

    Shell

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
+
+    def parse(self, response):
+        pass
