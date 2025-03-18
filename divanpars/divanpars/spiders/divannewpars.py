9e751c43be75c5ab3b9b66dea55c9db9fd30d5bb
Pavel N <npa2003@gmail.com>
2025-03-19 00:29:43 +0300

    1

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -7,4 +7,11 @@ class DivannewparsSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
 
     def parse(self, response):
-        pass
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+            yield {
+                'name': divan.css('div.lsooF span::text').get(),
+                'price': divan.css('div.pY3d2 span::text').get(),
+                'url': divan.css('a').attrib['href']
+            }
+
